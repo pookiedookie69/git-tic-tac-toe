9a0e11a9eb52fda5f8bc141f9c85cd4139befcd9
pookiedookie69 <128asasina@frhsd.com>
2026-09-14 10:45:45 -0400

    sending Class and Test

--- src/Class.java
+++ src/Class.java
@@ -1,7 +1,20 @@
-import java.util.*;
-
+import java.util.Scanner;
 public class Class {
-    Scanner scanner = new Scanner(System.in);
+    private Student [][] chart;
 
+    public Class(Student [][] chart) {
+        this.chart = chart;
+    }
+    public void intro (){
+        chart = new Student[6][4];
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("This is your current seat: " + seatChooser());
+
+    }
+    public String seatChooser (){
+        int row = (int) (Math.random()*6);
+        int col = (int) (Math.random()*4);
+        return ("["+row+","+col+"]");
+    }
 
 }
